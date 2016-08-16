158024dcf5ea54f783090b3f61f026692131d635
Luke Simons <lsimons@woof.fnal.gov>
2016-08-16 14:43:52 -0500

    Added ported LArLite Flash Matching code initially developed on develop branch of https://github.com/LukeSimons/laralgobj.git - LS

---
+++ lardataalg/PhotonLibrary/classes.h
@@ -0,0 +1,5 @@
+// #include "canvas/Persistency/Common/Wrapper.h"
+
+#include "lardataalg/PhotonLibrary/PhotonVisibilityService.h"
+#include "lardataalg/PhotonLibrary/PhotonLibrary.h"
+#include "lardataalg/PhotonLibrary/PhotonVoxels.h"
